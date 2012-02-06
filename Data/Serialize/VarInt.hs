f105e76e5e9b39e02e17e25d250fcdfa9efa85e8
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-02-06 17:22:22 +0400

    Add deserialization of varints

---
+++ Data/Serialize/VarInt.hs
@@ -0,0 +1,25 @@
+-- | Variable width integer encoding which are used in google protobuf
+module Data.Serialize.VarInt where
+
+import Control.Applicative
+
+import Data.Bits
+import Data.Serialize.Get
+import Data.Serialize.Put
+import Data.Word
+
+
+
+getVarWord :: (Bits a, Num a) => Get a
+getVarWord = do
+  w <- fromIntegral <$> getWord8
+  if not (testBit w 7)
+    then return w
+    else loop 7 (w .&. 0x7f)
+  where
+    loop n b = do
+      w <- fromIntegral <$> getWord8
+      if not (testBit w 7)
+        then return $! (w `shiftL` n) .|. b
+        else loop (n+7) (((w .&. 0x7f) `shiftL` n) .|. b)
+
