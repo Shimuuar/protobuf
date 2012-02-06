f474169c22e09c8c8f7d837fcecb72b3b392e4b5
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-02-06 16:10:58 +0400

    Few comments

--- Data/Protobuf/DataTree.hs
+++ Data/Protobuf/DataTree.hs
@@ -1,5 +1,11 @@
 -- | Tree which describe generated haskell data structures. It's then
 -- converted to haskell-src-exts syntax tree and pretty-printed
+--
+-- * @required@ is translated into ordinary field
+--
+-- * @optional@ is translated into 'Maybe'
+--
+-- * @repeated@ could be either 'Seq' or 'Vector'
 module Data.Protobuf.DataTree where
 
 import qualified Data.Map as Map
@@ -12,3 +18,15 @@ data DataTree = DataTree (Map String Module)
 
 -- | Single module
 data Module = Module
+
+
+
+-- | Haskell declaration of protobuf Enum
+data HsEnum = HsEnum TyName [(TyName,Integer)]
+              deriving Show
+
+
+-- | Haskell type or type constructor name. It must start from capital
+--   letter
+newtype TyName = TyName String
+                 deriving Show
