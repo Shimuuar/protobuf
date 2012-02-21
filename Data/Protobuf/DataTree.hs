a6ad7167880735e85b24af367057e23022a836ce
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-02-21 22:36:05 +0400

    Update data tree for generated haskell

--- Data/Protobuf/DataTree.hs
+++ Data/Protobuf/DataTree.hs
@@ -1,5 +1,10 @@
--- | Tree which describe generated haskell data structures. It's then
--- converted to haskell-src-exts syntax tree and pretty-printed
+-- | 
+-- Tree which describe generated haskell data structures. It's then
+-- converted to haskell-src-exts syntax tree and pretty-printed.
+--
+-- Each message and enum is put into separate module. This done in
+-- order to avoid ovelapping of record field. Currently record system
+-- of haskell is badly brocken.
 --
 -- * @required@ is translated into ordinary field
 --
@@ -11,19 +16,35 @@ module Data.Protobuf.DataTree where
 import qualified Data.Map as Map
 import           Data.Map   (Map)
 
+import Data.Protobuf.AST (PrimType(..), FieldTag(..) )
 
 
--- | Complete 
-data DataTree = DataTree (Map String Module)
-
--- | Single module
-data Module = Module
+-- | complete 
+data DataTree = DataTree (Map String HsModule)
 
 
+-- | Haskell module. It contains single data type which corresponds
+--   either to message or to enum.
+data HsModule 
+  = HsMessage TyName [HsField]
+  | HsEnum    TyName [(TyName,Integer)]
 
--- | Haskell declaration of protobuf Enum
-data HsEnum = HsEnum TyName [(TyName,Integer)]
-              deriving Show
+
+-- | Single field of haskell message
+data HsField 
+  = HsField HsType String FieldTag
+
+-- | Haskell type of field in message
+data HsType 
+  = HsSimple HsTypename         -- ^ Required data type
+  | HsMaybe  HsTypename         -- ^ Optional data type
+  | HsSeq    HsTypename         -- ^ Repeated data type as 'Seq'
+
+-- | Name of type. 
+data HsTypename
+  = HsBuiltin  PrimType
+  | HsUserType TyName
+
 
 
 -- | Haskell type or type constructor name. It must start from capital
