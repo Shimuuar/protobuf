2ed2d0392695b1438a3ae8426360e182317348c0
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-03-03 00:45:45 +0400

    Change API

--- Data/Protobuf/Classes.hs
+++ Data/Protobuf/Classes.hs
@@ -1,7 +1,7 @@
 module Data.Protobuf.Classes (
-    Default(..)
-  , PbEnum(..)
+    PbEnum(..)
   , Message(..)
+  , MessageEncode(..)
   ) where
 
 import Data.Word
@@ -9,34 +9,51 @@ import Data.Int
 import Data.Serialize.Get (Get)
 import Data.Serialize.Put (Put)
 
-
-class Default a where
-  def :: a
-  
-instance Default Bool   where def = False
-
-instance Default Word   where def = 0
-instance Default Word8  where def = 0
-instance Default Word16 where def = 0
-instance Default Word32 where def = 0
-instance Default Word64 where def = 0
-
-instance Default Int    where def = 0
-instance Default Int8   where def = 0
-instance Default Int16  where def = 0
-instance Default Int32  where def = 0
-instance Default Int64  where def = 0
-
-instance Default [a]    where def = []
-
-
-
--- | Type class for protobuf enums
+-- | Type class for protobuf enums. It's possible to convert them to
+--   'Int' and back
 class PbEnum a where
   fromPbEnum :: a   -> Int
   toPbEnum   :: Int -> a
 
--- | Serialization/deserialization of message
+-- | API for messages.
 class Message a where
+  -- | Default value
+  def :: a
+  -- | Merge messages. Default implementation picks last value
+  mergeMsg :: a -> a -> a
+  mergeMsg _ = id
+  {-# INLINE mergeMsg #-}
+
+
+
+instance Message Bool   where def = False
+
+instance Message Word   where def = 0
+instance Message Word8  where def = 0
+instance Message Word16 where def = 0
+instance Message Word32 where def = 0
+instance Message Word64 where def = 0
+
+instance Message Int    where def = 0
+instance Message Int8   where def = 0
+instance Message Int16  where def = 0
+instance Message Int32  where def = 0
+instance Message Int64  where def = 0
+
+instance Message a => Message (Maybe a) where
+  def = Nothing
+  mergeMsg Nothing  x        = x
+  mergeMsg x        Nothing  = x
+  mergeMsg (Just x) (Just y) = Just $ mergeMsg x y
+  {-# INLINE mergeMsg #-}
+
+instance Message [a] where 
+  def      = []
+  mergeMsg = (++)
+
+
+
+-- | Serialization/deserialization of message
+class MessageEncode a where
   getMessage :: Get a
   putMessage :: a -> Put
