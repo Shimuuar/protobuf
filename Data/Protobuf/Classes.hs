4a9fff153e8f1bfd0ea5109858cde37e5acf1aaa
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-03-07 21:09:36 +0400

    Update API

--- Data/Protobuf/Classes.hs
+++ Data/Protobuf/Classes.hs
@@ -1,7 +1,12 @@
 module Data.Protobuf.Classes (
-    PbEnum(..)
+    -- * Data
+    Required
+  , Val
+    -- * Classes
+  , PbEnum(..)
+  , Default(..)
   , Message(..)
-  , MessageEncode(..)
+  , MessageField(..)
   ) where
 
 import Data.Word
@@ -9,51 +14,104 @@ import Data.Int
 import Data.Serialize.Get (Get)
 import Data.Serialize.Put (Put)
 
+-- | Maybe-like data type for tracking whether field is set or not.
+data Required a 
+  = Present a
+  | NotSet
+
+instance Functor Required where
+  fmap f (Present x) = Present (f x)
+  fmap _ NotSet      = NotSet
+
+-- | 
+newtype Val a = Val a
+
+
+----------------------------------------------------------------
+-- Type classes
+----------------------------------------------------------------
+
 -- | Type class for protobuf enums. It's possible to convert them to
 --   'Int' and back
 class PbEnum a where
   fromPbEnum :: a   -> Int
   toPbEnum   :: Int -> a
 
--- | API for messages.
-class Message a where
+
+-- | Default value for field
+class Default a where
   -- | Default value
   def :: a
-  -- | Merge messages. Default implementation picks last value
-  mergeMsg :: a -> a -> a
-  mergeMsg _ = id
-  {-# INLINE mergeMsg #-}
-
-
-
-instance Message Bool   where def = False
-
-instance Message Word   where def = 0
-instance Message Word8  where def = 0
-instance Message Word16 where def = 0
-instance Message Word32 where def = 0
-instance Message Word64 where def = 0
-
-instance Message Int    where def = 0
-instance Message Int8   where def = 0
-instance Message Int16  where def = 0
-instance Message Int32  where def = 0
-instance Message Int64  where def = 0
-
-instance Message a => Message (Maybe a) where
-  def = Nothing
-  mergeMsg Nothing  x        = x
-  mergeMsg x        Nothing  = x
-  mergeMsg (Just x) (Just y) = Just $ mergeMsg x y
-  {-# INLINE mergeMsg #-}
-
-instance Message [a] where 
-  def      = []
-  mergeMsg = (++)
-
 
 
 -- | Serialization/deserialization of message
-class MessageEncode a where
-  getMessage :: Get a
-  putMessage :: a -> Put
+class Message a where
+  -- | Deserialize message
+  getMessage :: Get (a Required)
+  -- | Check that all required fields are present
+  checkReq   :: Monad m => a Required -> m (a Val)
+  -- | Serialize message
+  putMessage :: (a Val) -> Put
+
+
+
+----------------------------------------------------------------
+-- Implementation type classes
+
+class MessageField f where
+  mergeField :: f -> f -> f
+  mergeField _ x = x
+  {-# INLINE mergeField #-}
+
+
+
+----------------------------------------------------------------
+-- Instances for MessageField
+
+instance MessageField Bool
+instance MessageField Word32
+instance MessageField Word64
+instance MessageField Int32
+instance MessageField Int64
+instance MessageField [a] where
+  mergeField = (++)
+
+
+instance MessageField a => MessageField (Maybe a) where
+  mergeField (Just a) (Just b) = Just $ mergeField a b
+  mergeField Nothing  x        = x
+  mergeField x        Nothing  = x
+
+instance MessageField a => MessageField (Required a) where
+  mergeField (Present a) (Present b) = Present $ mergeField a b
+  mergeField NotSet  x       = x
+  mergeField x       NotSet  = x
+
+instance MessageField a => MessageField (Val a) where
+  mergeField (Val a) (Val b) = Val $ mergeField a b
+
+
+
+----------------------------------------------------------------
+-- Instances for Default
+
+instance Default Bool   where def = False
+
+instance Default Word   where def = 0
+instance Default Word8  where def = 0
+instance Default Word16 where def = 0
+instance Default Word32 where def = 0
+instance Default Word64 where def = 0
+
+instance Default Int    where def = 0
+instance Default Int8   where def = 0
+instance Default Int16  where def = 0
+instance Default Int32  where def = 0
+instance Default Int64  where def = 0
+
+instance Default (Required a) where
+  def = NotSet
+instance Default (Maybe a) where
+  def = Nothing
+instance Default [a] where
+  def = []
