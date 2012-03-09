b116fc14206cc920b6304a8564ca9b3183917e09
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-03-09 21:53:38 +0400

    Working decoding

--- Data/Serialize/Protobuf.hs
+++ Data/Serialize/Protobuf.hs
@@ -4,25 +4,34 @@ module Data.Serialize.Protobuf (
     WireTag(..)
   , getWireTag
   , skipUnknownField
+  , getPacked
+  , getPbString
   ) where
 
+import Control.Applicative
 import Control.Monad
 import Data.Bits
 
+import Data.Serialize
 import Data.Serialize.Get
 import Data.Serialize.Put
 import Data.Serialize.VarInt
+import qualified Data.Sequence as Seq
+import           Data.Sequence   (Seq)
+import Debug.Trace
 
 
 getVarInt :: Get Int
-getVarInt = undefined
+getVarInt = fromIntegral <$> getVarInt64
 
 data WireTag = WireTag {-# UNPACK #-} !Int {-# UNPACK #-} !Int
+               deriving Show
+
 
 getWireTag :: Get WireTag
 getWireTag = do
   i <- getVarInt
-  return $! WireTag (0x07 .&. i) (i `shiftR` 3)
+  return $! WireTag (i `shiftR` 3) (0x07 .&. i)
 
 getSequence :: Get a -> Get [a]
 getSequence getter = do
@@ -30,6 +39,29 @@ getSequence getter = do
   replicateM n getter
 
 skipUnknownField :: WireTag -> Get ()
-skipUnknownField w = do
-  return ()
+skipUnknownField (WireTag _ t) =
+  case t of
+    0 -> void getVarInt
+    1 -> skip 8
+    2 -> skip =<< getVarInt
+    3 -> fail "Groups are not supported"
+    4 -> fail "Groups are not supported"
+    5 -> skip 4 
+    _ -> fail "Bad wire tag"
 
+getPacked :: Get a -> Get (Seq a)
+getPacked getter = do
+  n <- getVarInt
+  Seq.fromList `fmap` replicateM n getter
+
+getPbString :: Get String
+getPbString = do
+  n <- getVarInt
+  isolate n $ repeatedly
+
+repeatedly :: Get String
+repeatedly = do
+  f <- isEmpty
+  if f
+    then return []
+    else (:) <$> get <*> repeatedly
